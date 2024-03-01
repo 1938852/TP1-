f1065efedf1408601d3383191bf2564ce2d4885f
1938852 <112107093+1938852@users.noreply.github.com>
2024-03-01 18:37:44 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Atelier2C6_101_2024;
+using Atelier2C6_101_2024.Connect4;
+
+namespace TP1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Util.Titrer("Partie de Connect4");
+            Util.Pause();
+
+            PartieConnect4 pConn4 = new PartieConnect4();
+            pConn4.Jouer();
+        }
+    }
+}
---
+++ util.cs
@@ -0,0 +1,111 @@
+﻿//-------------------------------
+//  Fichier Util.cs
+//  Auteur: Alain Martel
+//  Création: 2024-01-31 
+//-------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atelier2C6_101_2024
+{
+    internal class Util
+    {
+        public static string[] _noms = new string[] {"Esteban", "Etienne", "Raphael", "Liliane", "Samuel", "Félix", "Georges",
+        "Charles-Etienne", "Fernando", "Nizar"};
+
+        public static Random rdm = new Random();
+
+        public enum ERREUR
+        {
+          AUCUNE_ERREUR,
+          ERR_DATE_NULLE,
+          ERR_DATE_INCOMPLETE,
+          ERR_ANNEE_HORS_LIMITE,
+          ERR_ANNEE_INVALIDE,
+          ERR_MOIS_HORS_LIMITE,
+          ERR_MOIS_INVALIDE,
+          ERR_JOUR_HORS_LIMITE,
+          ERR_JOUR_INVALIDE
+        }
+
+
+
+    //-------------------------------
+    //
+    //-------------------------------
+    public static void ViderEcran()
+        {
+            Console.Clear();
+            Console.WriteLine("\x1b[3J");
+        }
+
+        //-------------------------------
+        //
+        //-------------------------------
+        public static void Titrer(string leTitre)
+        {
+            ViderEcran();
+            Console.WriteLine(leTitre);
+            for(int i = 0; i < leTitre.Length; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();    
+        }
+
+        //-------------------------------
+        //
+        //-------------------------------
+        public static char SaisirChar()
+        {
+            ConsoleKeyInfo keyInfo;
+            keyInfo = Console.ReadKey();
+            return keyInfo.KeyChar;
+        }
+
+        //-------------------------------
+        //
+        //-------------------------------
+        public static double SaisirUnDouble(string nomDuDouble)
+        {
+            Console.WriteLine(nomDuDouble + ":");
+            string? input = Console.ReadLine();
+
+
+            if (double.TryParse(input, out double resultat))
+            {
+                return resultat;
+            }
+            return 0.0;
+        }
+
+        //-------------------------------
+        //
+        //-------------------------------
+        public static void Pause()
+        {
+            Console.WriteLine(" appuyer une touche...");
+            Console.ReadKey();
+        }
+
+        //-------------------------------
+        //
+        //-------------------------------
+        public static void SepST(string s)
+        {
+            Console.WriteLine("------------" + s + "------------");
+        }
+        //-------------------------------
+        //
+        //-------------------------------
+        public void Sep(string s)
+        {
+            Console.WriteLine("***********" + s + "************");
+        }
+
+    }
+}
